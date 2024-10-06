99c3ff5be3a917f83008a0fc0840af73e30dd196
Robbie <robbiethewadley@gmail.com>
2024-10-06 01:35:31 -0600

    document dependencies

--- ci/project.test.ts
+++ ci/project.test.ts
@@ -1,3 +1,26 @@
+import { expect, test } from "bun:test";
+import project from "../package.json" assert { type: "json" };
+
 /**
- * stub for now
+ * all dependencies should be documented
+ * and no non-dependencies should be documented
  */
+const deps: Record<string, string> = {
+	...project.dependencies,
+	...project.devDependencies,
+};
+const depDocs: Record<string, string> = {
+	...project["//dependencies"],
+	...project["//devDependencies"],
+};
+
+for (const dep of Object.keys(deps)) {
+	test(`dependency ${dep} should be documented`, () => {
+		expect(depDocs[dep]).toBeTruthy();
+	});
+}
+for (const dep of Object.keys(depDocs)) {
+	test(`documented dependency ${dep} should be in dependencies`, () => {
+		expect(deps[dep]).toBeTruthy();
+	});
+}
