21e41de2bc800d7fcd607231086f8c4ca70384a4
Robbie <robbiethewadley@gmail.com>
2024-10-06 01:07:28 -0600

    port things from new-connect

---
+++ ci/check-bundle-size.ts
@@ -0,0 +1,59 @@
+/**
+ * Run a build, then calculate the total size for all pages and assets
+ */
+
+import { $, Glob } from "bun";
+
+const buildOutput = $`bun run build`.lines();
+const extractSizeRegex = /([.\d]+) ([kbmg]?B)/;
+let isFinished = false;
+let sizeInBytes = 0;
+
+// get the JS size for each page
+for await (const line of buildOutput) {
+	// wait for build to finish
+	if (line.includes("First Load JS")) isFinished = true;
+	if (!isFinished) continue;
+
+	const included = line.includes("/") && !line.includes("chunks");
+	const shared = line.includes("shared by all");
+	if (!included && !shared) continue;
+
+	// the first size listed is the one we want (for pages, that will exclude the shared chunk - we count it separately)
+	const [count, unit] = extractSizeRegex.exec(line)?.slice(1) ?? [];
+	if (!count || !unit) continue;
+
+	if (unit === "B") sizeInBytes += Number.parseInt(count);
+	else if (unit === "kB") sizeInBytes += Number.parseInt(count) * 1024;
+	else if (unit === "MB") sizeInBytes += Number.parseInt(count) * 1024 * 1024;
+	else if (unit === "GB")
+		sizeInBytes += Number.parseInt(count) * 1024 * 1024 * 1024;
+	else throw new Error(`Unknown unit ${unit}`);
+}
+
+// also measure the gzipped size of every asset in the build.
+const allAssets = await new Glob("**/*.{css,svg,png,jpeg,jpg,webp}");
+for await (const file of allAssets.scan(".next/static")) {
+	const size = await $`gzip -c .next/static/${file} | wc -c`.text();
+	sizeInBytes += Number.parseInt(size);
+}
+// worth noting it doesn't really make sense to include the public folder in the size -
+// but that's how the comma repo does it, so we'll do the same for the sake of comparison
+for await (const file of allAssets.scan("public")) {
+	const size = await $`gzip -c public/${file} | wc -c`.text();
+	sizeInBytes += Number.parseInt(size);
+}
+
+const sizeInKB = sizeInBytes / 1024;
+console.log(`Bundle size is ${Math.round(sizeInKB)} KB`);
+
+// check if the bundle size is within the expected range
+if (sizeInKB < 200) {
+	console.log("Bundle size lower than expected, let's lower the limit!");
+	process.exit(0); // TODO - this should be a non-zero exit code
+}
+
+if (sizeInKB > 325) {
+	console.log("Exceeded bundle size limit!");
+	process.exit(1);
+}
---
+++ ci/check-lines.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+cd ..
+
+COUNT="$(find app/ -type f | xargs wc -l | tail -n 1 | awk '{print $1}')"
+echo "$COUNT total lines"
+
+if [ "$COUNT" -gt 5000 ]; then
+  echo "Exceeded line limit!"
+  exit 1
+fi
