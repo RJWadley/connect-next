90821000f9d3a9cdb1d113fb858173ba3054e43d
Robbie <robbiethewadley@gmail.com>
2024-10-08 03:11:36 -0600

    fix auth on cloudflare

--- app/(api)/config.ts
+++ app/(api)/config.ts
@@ -2,4 +2,7 @@ export const ATHENA_URL = "https://athena.comma.ai";
 export const BASE_URL = "https://api.comma.ai";
 export const BILLING_URL = "https://billing.comma.ai";
 // CF_PAGES_URL is a cloudflare env value
-export const getService = () => process.env.CF_PAGES_URL || "localhost:3000";
+export const getService = () =>
+	process.env.CF_PAGES_URL
+		? new URL(process.env.CF_PAGES_URL).hostname
+		: "localhost:3000";
